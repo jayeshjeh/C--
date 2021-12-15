6088f182e27ee5f5977495a725dbd16e8d51185c
jayeshjeh <jay.parmar.11169@gmail.com>
2021-12-15 18:29:36 +0530

    commit

---
+++ Learn/enum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+enum day { mon=10, tue, wed=34, thur, fri, sat, sun};
+
+int main()
+{
+    day d;
+    d = tue;
+
+    cout<<fri<<"\n";
+
+    cout<<tue;
+
+    return 0;
+}  
